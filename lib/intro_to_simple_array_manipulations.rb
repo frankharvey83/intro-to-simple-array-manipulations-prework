88677470bf80eaaa5756cb93fee03c2a42c12556
Frank H Mcmanus <frank@thefrankharvey.com>
2018-02-28 02:28:05 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,10 +18,12 @@ def using_shift(arr)
   arr.shift()
 end
 
-def shift_with_args
+def shift_with_args(arr)
+  arr.shift(2)
 end
 
-def using_concat
+def using_concat(arr1, arr2)
+  arr1.concat arr2
 end
 
 def using_insert
